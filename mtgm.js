1fc688fd190a56c28225e942d183201a01a434c2
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-07-25 22:57:09 +0200

    pre edit version

---
+++ mtgm.js
@@ -0,0 +1,72 @@
+﻿// Mailto: Gmail™   -  By Chris (i.like.sleeping@gmail.com)
+// In theory, should comply with RFC2368 - if you find any bugs, let me know :)
+
+// Get url parameters.
+function getParam(u,p) {
+	var r = new RegExp(p + "=[^&]*","i")
+	var m = u.match(r);
+	return (m) ? m[0].replace(p + '=','') : '';
+}
+
+// Convert string values from localStorage to boolean
+function toBool(s) {
+	if (s === "false")	return false;
+	else 				return true;
+}
+
+// Set link to go to Gmail when clicked
+function setLink(e) {
+	var x = e.href;
+	x = x.replace(/'/ig,'%27');
+	var to = x.match(/mailto:[^?]*/i)[0].replace(/mailto:/i,'');
+	var to2 = getParam(x,"to");
+	to = (to2) ? to2 + '; ' + to : to;
+	var bcc = (allowBcc == true) ? getParam(x,"bcc") : '';
+	x = x.replace(/(bcc=)/ig,'');
+	var cc = getParam(x,"cc");
+	var su = getParam(x,"subject");
+	var body = getParam(x,"body");
+	
+	var gLink = (useGApps == true) ? 'https://mail.google.com/a/' + gaDomain + '/mail/' : 'https://mail.google.com/mail/';
+	var emaillink = gLink + '?view=cm&tf=1&to=' + to + "&cc=" + cc + "&bcc=" + bcc + "&su=" + su + "&body=" + body;
+
+	if (useWindow == true){
+	e.setAttribute("onclick", "window.open('" + emaillink + "','_blank','location=yes,menubar=yes,resizable=yes,width=800,height=600');return false;");
+	} else {
+	e.setAttribute("onclick", "window.open('" + emaillink + "','_blank');return false;");
+	}
+}
+
+// Find mailto links
+function findLinks() {
+	for (var z = 0; z < document.links.length; z++) {
+		if (document.links[z].href.match(/^mailto:/i)) {
+			setLink(document.links[z]);
+		}
+	}
+}
+
+// Get settings
+var useGApps, gaDomain, allowBcc, useWindow;
+chrome.extension.sendRequest({storage: "getSettings"}, function(r) {
+	useGApps = r.storage.useGApps;
+	if (useGApps)
+		gaDomain = r.storage.gaDomain;
+	allowBcc = r.storage.allowBcc;
+	useWindow = r.storage.useWindow;
+	findLinks();
+});
+
+// Called when an element is added after page load
+function inserted(e) {
+	if (e.target.nodeName == "A" && e.target.href.match(/^mailto:/i))
+		setLink(e.target);
+	var nodes = e.target.getElementsByTagName('A');
+	for (var z = 0; z < nodes.length; z++){
+		if (nodes[z].href.match(/^mailto:/i))
+			setLink(nodes[z]);
+	}
+}
+
+// Watch for inserted mailto links
+document.addEventListener('DOMNodeInserted', inserted, false );
